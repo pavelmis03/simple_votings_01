fc8139b711d5e99ecf0f71b07a3b21611c6f5197
Sorochan Ilya <super.ilya.66@gmail.com>
2020-01-14 19:58:24 +0300

    Added complaints model

--- vote/models.py
+++ vote/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Constants:
+    COMPLAINTS_TITLE_SIZE = 100
+    COMPLAINTS_TEXT_SIZE = 500
+    COMPLAINTS_STATUS = (
+        ('U', 'under_consideration'),
+        ('R', 'rejected'),
+        ('A', 'accepted'),
+    )
+
+
+class Complaints(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=1, choices=Constants.COMPLAINTS_STATUS)
+    title = models.CharField(max_length=Constants.COMPLAINTS_TITLE_SIZE)
+    text = models.CharField(max_length=Constants.COMPLAINTS_TEXT_SIZE)
+    created_at = models.DateTimeField()
