50b715ff22b130139f0badaa7b40a9ee5acbe833
Pavel_Mishakov <pavelmis03@gmail.com>
2020-01-21 22:05:41 +0300

    created main menu navBar

--- vote/views.py
+++ vote/views.py
@@ -1,21 +1,26 @@
-import datetime
-
+from datetime import datetime
+import random
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 
-def get_base_context():
+def get_base_context(request):
     context = {
-        'menu': [
-            {'link': '/', 'text': 'Главная'},
-            {'link': '/test', 'text': 'Несуществующая'},
+        'MenuMain': [
+            {'punct_link': '/', 'punct_text': 'Main'},
         ],
-        'current_time': datetime.datetime.now(),
+        'current_time': datetime.now(),
     }
+    context['MenuMainAnon'] = []
+    if (request.user.is_anonymous):
+        context['MenuMainAnon'].append({'punct_link': '/test', 'punct_text': 'SignIn'})
+        context['MenuMainAnon'].append({'punct_link': '/test', 'punct_text': 'Register'})
     return context
 
 
 def index_page(request):
-    context = get_base_context()
+    context = get_base_context(request)
     context['title'] = 'Главная страница - simple votings'
     context['main_header'] = 'Simple votings'
     return render(request, 'index.html', context)
